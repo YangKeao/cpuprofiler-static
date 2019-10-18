030442ea74615af7698e559a9259396b3f8cb4c4
Yang Keao <keao.yang@yahoo.com>
2019-10-18 16:47:11 +0800

    boost up this repo
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

---
+++ build.rs
@@ -0,0 +1,136 @@
+use std::path::{PathBuf, Path};
+use std::{io, fs};
+use std::process::Command;
+
+fn is_directory_empty<P: AsRef<Path>>(p: P) -> Result<bool, io::Error> {
+    let mut entries = fs::read_dir(p)?;
+    Ok(entries.next().is_none())
+}
+
+fn prepare_gperftool() {
+    let libs = vec![
+        "third_party/gperftools",
+        "third_party/libunwind",
+    ];
+
+    for lib in libs {
+        if is_directory_empty(lib).unwrap_or(true) {
+            panic!(
+                "Can't find library {}. You need to run `git submodule \
+                 update --init --recursive` first to build the project.",
+                lib
+            );
+        }
+    }
+}
+
+fn build_gperftool(source_root: &PathBuf) -> std::io::Result<PathBuf> {
+    prepare_gperftool();
+
+    let target_gperftool_source_dir = {
+        let mut source_root = source_root.clone();
+        source_root.push("gperftools");
+        source_root
+    };
+
+    let mut autogen = Command::new("./autogen.sh")
+        .current_dir(&target_gperftool_source_dir)
+        .spawn()?;
+    autogen.wait()?;
+
+    let mut configure = Command::new("./configure")
+        .args(&[
+            "--disable-heap-profiler",
+            "--disable-heap-checker",
+            "--disable-debugalloc",
+            "--disable-shared",
+        ])
+        .current_dir(&target_gperftool_source_dir)
+        .spawn()?;
+    configure.wait()?;
+
+    let cpu_num = num_cpus::get();
+    let mut make = Command::new("make")
+        .args(&[format!("-j{}", cpu_num), "--keep-going".to_owned()])
+        .current_dir(&target_gperftool_source_dir)
+        .spawn()?;
+    make.wait()?;
+
+    Ok(target_gperftool_source_dir)
+}
+
+fn build_unwind(source_root: &PathBuf) -> std::io::Result<PathBuf> {
+    prepare_gperftool();
+
+    let target_unwind_source_dir = {
+        let mut source_root = source_root.clone();
+        source_root.push("libunwind");
+        source_root
+    };
+
+    let mut autogen = Command::new("./autogen.sh")
+        .current_dir(&target_unwind_source_dir)
+        .spawn()?;
+    autogen.wait()?;
+
+    let mut configure = Command::new("./configure")
+        .args(&["--disable-shared", "--disable-minidebuginfo", "--disable-zlibdebuginfo"])
+        .current_dir(&target_unwind_source_dir)
+        .spawn()?;
+    configure.wait()?;
+
+    let cpu_num = num_cpus::get();
+    let mut make = Command::new("make")
+        .args(&[format!("-j{}", cpu_num), "--keep-going".to_owned()])
+        .current_dir(&target_unwind_source_dir)
+        .spawn()?;
+    make.wait()?;
+
+    Ok(target_unwind_source_dir)
+}
+
+fn copy_source_files() -> std::io::Result<PathBuf> {
+    let third_party_source_dir = {
+        let mut current =  std::env::current_dir()?;
+        current.push("third_party");
+        current
+    };
+
+    let target_third_party_source_dir: PathBuf = {
+        let out_dir: String = std::env::var("OUT_DIR").unwrap();
+        PathBuf::from(format!("{}/third_party", out_dir))
+    };
+
+    let mut copy = Command::new("cp")
+        .args(&[
+            "-r",
+            "--remove-destination",
+            "--no-target-directory",
+            &format!("{}", third_party_source_dir.display()),
+            &format!("{}", target_third_party_source_dir.display()),
+        ])
+        .spawn()?;
+    copy.wait()?;
+
+    Ok(target_third_party_source_dir)
+}
+
+fn main() -> std::io::Result<()> {
+    let source_root = copy_source_files()?;
+
+    let unwind = build_unwind(&source_root)?;
+    let gperftool = build_gperftool(&source_root)?;
+
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=static=profiler");
+    println!("cargo:rustc-link-lib=static=unwind");
+    println!(
+        "cargo:rustc-link-search=native={}/src/.libs",
+        unwind.display()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}/.libs",
+        gperftool.display()
+    );
+    Ok(())
+}
